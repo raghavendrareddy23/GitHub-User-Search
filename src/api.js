3bc42f55e8dc5ca4a8a2b45f2d9113f7760697ef
raghavendrareddy23 <raghavendrareddy.munagala23@gmail.com>
2024-01-25 00:34:16 +0530

    modifications in api

--- src/api.js
+++ src/api.js
@@ -1,14 +1,12 @@
-// api.js
 import axios from 'axios';
-const accessToken = 'YOUR_PERSONAL_ACCESS_TOKEN';
+
 const baseURL = 'https://api.github.com';
 
 const api = axios.create({
   baseURL,
   headers: {
-    Authorization: `Bearer ${accessToken}`, // Replace with your access token
+    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
   },
 });
-
 
 export default api;
