08ffc0ceb9edc056a354691c58d8f9a258106b92
raghavendrareddy23 <raghavendrareddy.munagala23@gmail.com>
2024-01-25 00:18:34 +0530

    api updates

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 // api.js
 import axios from 'axios';
-
+const accessToken = 'YOUR_PERSONAL_ACCESS_TOKEN';
 const baseURL = 'https://api.github.com';
 
 const api = axios.create({
   baseURL,
   headers: {
-    Authorization: `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`, // Replace with your access token
+    Authorization: `Bearer ${accessToken}`, // Replace with your access token
   },
 });
 
